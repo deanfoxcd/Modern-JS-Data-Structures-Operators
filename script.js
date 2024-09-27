7c9a9fbc344cb30ea57cf681a3d7fb9035e9ea2c
Dean Fox <deanfoxcd@gmail.com>
2024-09-27 19:48:06 -0400

    add enhanced obj literals

--- script.js
+++ script.js
@@ -47,28 +47,39 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+// ES6 Enhanced Object Literal
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // Only need to write the object name in ES6 if the names are the same
+  openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  // Functions before ES6
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  // Functions after ES6
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -89,6 +100,7 @@ const restaurant = {
 };
 
 // **FOR/OF LOOP**
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 // Logs each item in the array
@@ -109,6 +121,7 @@ for (const [i, el] of menu.entries()) {
   // destructures the menu.entries output. i is index, el is menu item
   console.log(`${i + 1}: ${el}`);
 }
+  */
 
 // **CODING CHALLENGE 1**
 /*
